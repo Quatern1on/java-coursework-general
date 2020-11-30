8cc63224fbbfb909fc4a7f56cf8dcb020cb9ef7f
Vadik <turchenkovadim2000@gmail.com>
2020-11-30 14:22:56 +0200

    Add hibernate implementation for CarModelService and CarBrandService

---
+++ src/main/java/edu/javavt18/service/CarBrandHibernateServiceImpl.java
@@ -0,0 +1,36 @@
+package edu.javavt18.service;
+
+import edu.javavt18.dao.CarBrandDAO;
+import edu.javavt18.model.CarBrand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service("carBrandHibernateService")
+@Transactional(readOnly = false, value = "hibernateTransactionManager")
+public class CarBrandHibernateServiceImpl implements CarBrandService {
+
+    @Autowired
+    @Qualifier("getCarBrandHibernateDAO")
+    private CarBrandDAO carBrandDAO;
+
+
+    public void saveOrUpdate(CarBrand item) {
+        carBrandDAO.saveOrUpdate(item);
+    }
+
+    public void delete(int itemId) {
+        carBrandDAO.delete(itemId);
+    }
+
+    public CarBrand get(int itemId) {
+        return carBrandDAO.get(itemId);
+    }
+
+    public List<CarBrand> list() {
+        return carBrandDAO.list();
+    }
+}
